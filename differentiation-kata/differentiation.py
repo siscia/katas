1e980b3907bea0f88da15005207028b0d5c5d3a3
Simone Mosciatti <mweb.sim@gmail.com>
2019-06-25 19:11:49 +0200

    fix some parsing by Mauro

--- differentiation-kata/differentiation.py
+++ differentiation-kata/differentiation.py
@@ -15,23 +15,46 @@ import unittest
 def differentiate(s):
     return s
 
+#import re
+#tokens = ['(', ')', 'x', '*', '+', '-', '/', 'sin', 'cos', '^', 'ln', r'[0-9]+', r'\w+']
+#tokens = [re.compile(i) for i in tokens]
+
 def parse(text):
-    return [ int(x) if x.isdigit() else x for x in text[1:-1].split()]
+    text = text.replace('(', '( ').replace(')', ' )')
+    tokens = text.split()
+    retval = []
+    it = iter(tokens)
+    def internal_parse(it):
+        ret = []
+        for tok in it:
+            print(tok)
+            if tok == '(':
+                ret.append(internal_parse(it))
+            elif tok == ')':
+                return ret
+            elif tok.isdigit():
+                ret.append(int(tok))
+            elif not tok:
+                continue
+            else:
+                ret.append(tok)
+        return ret
+    return internal_parse(it)[0]
 
 def dump(text):
     return text
 
 class TestGoal(unittest.TestCase):
-    def test_working(self):
-        self.assertEqual(1 + 1, 2)
+    #def test_working(self):
+    #    self.assertEqual(1 + 1, 2)
 
     def test_to_datastructure(self):
         self.assertEqual(dump(parse("(* x 2)")), ['*', 'x', 2])
         self.assertEqual(dump(parse("(* x 12)")), ['*', 'x', 12])
         self.assertEqual(dump(parse("(+ (* x 12) (* x 2))")), ['+' ['*', 'x', 12], ['*', 'x', 2]])
 
-    def test_goal(self):
-        self.assertEqual(differentiate("(^ x 2)"), "(* 2 x)")
+    #def test_goal(self):
+    #    self.assertEqual(differentiate("(^ x 2)"), "(* 2 x)")
 
 
 # to run `python3 differentation.py`
